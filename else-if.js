3318e6264c10921860fae73c71ad8576682394ce
mazedabegum <mazedabegum523@gmail.com>
2024-02-20 14:26:42 +0600

    first commit

---
+++ else-if.js
@@ -0,0 +1,13 @@
+var boy = true;
+var girl = true;
+var mother = true;
+var father = true;
+
+if(boy = true){
+    console.log("boy name Rafayat");
+}else(girl = true){
+    console.log(girl);
+    if(mother = true){
+        console.log(come);
+    }
+}
---
+++ if-else.js
@@ -0,0 +1,8 @@
+var flower = 1500;
+var dress = 2000;
+var shoe = 500; 
+mybudget = 3000;
+
+if( flower < mybudget){
+ console.log( "give me flower")
+}else("give me dress")
